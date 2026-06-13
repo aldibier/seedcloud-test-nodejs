01293073cd5c61e5592ac28b3bca15b2bd1b0f0a
Seedcloud <amorales@seedem.co>
2026-06-13 07:02:54 -0500

    Initial Lagoon test project

---
+++ index.js
@@ -0,0 +1,5 @@
+const http = require('http');
+http.createServer((req, res) => {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Seedcloud OK\n');
+}).listen(3000, () => console.log('Server running on port 3000'));
